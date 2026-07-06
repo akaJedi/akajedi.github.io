6e4fbd2127000fbd14bc861fb66e45c076b8fb2f
Denis Tolochko <d@f12.biz>
2026-07-05 17:28:36 -0700

    Fix Metricool JavaScript format

--- static/js/metricool.js
+++ static/js/metricool.js
@@ -1,1 +1,17 @@
-<script>function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"9c18b5b84fe2564dc527079e74b7e1b4"})});</script>
+function loadScript(callback) {
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+
+  script.type = "text/javascript";
+  script.src = "https://tracker.metricool.com/resources/be.js";
+  script.onreadystatechange = callback;
+  script.onload = callback;
+
+  head.appendChild(script);
+}
+
+loadScript(function () {
+  beTracker.t({
+    hash: "9c18b5b84fe2564dc527079e74b7e1b4"
+  });
+});
